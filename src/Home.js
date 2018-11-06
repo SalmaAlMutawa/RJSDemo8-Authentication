0bc481c33022db00e87626588c6ed4fc5951d524
Octowl <alsaff1987@gmail.com>
2018-11-06 16:19:16 +0300

    Conditional rendering

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 const Home = props => {
+  const { user } = props;
   return (
     <div className="row d-flex align-items-center" style={{ height: "80vh" }}>
       <Link to="/garbage" className="btn btn-lg btn-outline-secondary mx-auto">
         Random Garbage
       </Link>
-      <Link to="/treasure" className="btn btn-lg btn-warning mx-auto">
-        TREASURE
-      </Link>
+      {user && (
+        <Link to="/treasure" className="btn btn-lg btn-warning mx-auto">
+          TREASURE
+        </Link>
+      )}
     </div>
   );
 };
 
-export default Home;
+const mapStateToProps = state => ({
+  user: state.auth.user
+});
+
+export default connect(mapStateToProps)(Home);
